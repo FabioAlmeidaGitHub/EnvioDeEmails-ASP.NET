4993b3fa3d44ae8fa5a42b92958ebb02469d8531
FabioAlmeidaTI <fabioti100@gmail.com>
2021-10-10 13:50:45 -0300

    init

---
+++ EnvioDeEmails/Models/EmailViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EnvioDeEmails.Models
+{
+    public class EmailViewModel
+    {
+        public string Email { get; set; }
+        public string Assunto { get; set; }
+        public string Mensagem { get; set; }
+    }
+}
